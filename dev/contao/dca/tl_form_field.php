ec30b11bb7b7268cc474e071c145ab2df5b4ec15
jerome wein <jeromea.wein@gmail.com>
2020-08-19 14:27:27 +0200

    smaller stylings and BA-List alterings

---
+++ dev/contao/dca/tl_form_field.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['label']['eval']['maxLength'] = 510;
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['label']['sql'] = "varchar(510) NOT NULL default ''";
